2fd16c83cf3e52835e3f090a3fe74c6d364cab1d
VaneGarciaYara <vanegarciay@gmail.com>
2017-07-10 22:21:12 -0400

    - Ya se tiene la funcionalidad total, ya se puede eliminar y mover a completado sin necesidad de recargar la pagina.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,35 @@
 $(document).ready(function(){
     var tareas = new Array();
+    var tareas_terminadas = new Array();
     
     cargarTareasExistentes();
 
     /* Cuando hace click en el botòn enviar */
     $("button#enviar").click(function(e){
         e.preventDefault();
-        var nueva_tarea = $("#tarea").val();
-
-        if(esValidaLaNuevaTarea(nueva_tarea)) {
-            guardarNuevaTarea(nueva_tarea);
-            mostrarNuevaTareaEnListado(nueva_tarea);
-            limpiarInput();
-        } else {
-            mostrarErrorEnNuevaTarea();
+        enviar();
+    });
+    $("input#tarea").keydown(function(e){
+        var code = e.keyCode || e.which;
+        if(code == 13) { //Enter keycode
+            enviar();
         }
     });
 
     /* Cuando se ponga en checked la tarea */
-    $("#padre-lista input").bind("click", function(e){ // Selecciona todas las tareas y detecta cuando se le hace click (pone chulito)
-        console.log("Clicking the checkbox");
-        e.preventDefault();
+    $("#padre-lista").on("click","li input", function(){ // Selecciona todas las tareas y detecta cuando se le hace click (pone chulito)
         marcarTareaCompletada($(this));
     });
 
     /* Eliminar por medio de la X las tareas del To Do Items */
-    $("#padre-lista button").click(function(e){ // Selecciona todas las tareas y detecta cuando se le hace click (pone chulito)
+    $("#padre-lista").on("click", "button", function(e){ // Selecciona todas las tareas y detecta cuando se le hace click (pone chulito)
         e.preventDefault();
-        eliminarTarea(this);
+        eliminarTarea(this, "pendientes");
+    });
+
+    $("#padre-completed").on("click", "button", function(e){ // Selecciona todas las tareas y detecta cuando se le hace click (pone chulito)
+        e.preventDefault();
+        eliminarTarea(this, "completadas");
     });
 
     function cargarTareasExistentes() {
@@ -36,6 +38,24 @@ $(document).ready(function(){
             tareas.forEach(function(tarea, index) {
                 $("#padre-lista").append('<li><input type="checkbox" class="filled-in" id="'+index+'"><label for="'+index+'">' +tarea+'</label><button>X</button></li>');
             });
+        } 
+        if (localStorage.length > 1) {
+            tareas_terminadas = JSON.parse(localStorage.getItem("tareas_terminadas"));
+            tareas_terminadas.forEach(function(tarea, index) {
+                $("#padre-completed").append('<li><input type="checkbox" checked disabled class="filled-in" id="'+index+'"><label for="'+index+'">' +tarea+'</label><button>X</button></li>');
+            });
+        }
+    }
+
+    function enviar() {
+        var nueva_tarea = $("#tarea").val();
+
+        if(esValidaLaNuevaTarea(nueva_tarea)) {
+            guardarNuevaTarea(nueva_tarea);
+            mostrarNuevaTareaEnListado(nueva_tarea);
+            limpiarInput();
+        } else {
+            mostrarErrorEnNuevaTarea();
         }
     }
 
@@ -56,6 +76,11 @@ $(document).ready(function(){
         localStorage.setItem("tareas", JSON.stringify(tareas));
     }
 
+    function guardarTareaTerminada(tarea_terminada) {
+        tareas_terminadas.push(tarea_terminada);
+        localStorage.setItem("tareas_terminadas", JSON.stringify(tareas_terminadas));
+    }
+
     function mostrarNuevaTareaEnListado(nueva_tarea) {
         var id_nueva_tarea = tareas.length;
         $("#padre-lista").append('<li><input type="checkbox" class="filled-in" id="'+id_nueva_tarea+'"><label for="'+id_nueva_tarea+'">' +nueva_tarea+'</label><button>X</button></li>');
@@ -70,6 +95,8 @@ $(document).ready(function(){
         var tarea = $(checkbox).parent("li").find("label").text(); // Trae el id especifico de cada li de tarea.
         var index = tareas.indexOf(tarea);
 
+        guardarTareaTerminada(tarea);
+
         $("#padre-completed").append('<li><input type="checkbox" checked disabled class="filled-in" id="'+index+'"><label for="'+index+'">' +tarea+'</label><button>X</button></li>');
         tareas.splice(index, 1);
         localStorage.setItem("tareas", JSON.stringify(tareas));
@@ -77,12 +104,17 @@ $(document).ready(function(){
         $(checkbox).parent("li").remove(); //Selecciona la tarea en especifica a la que se le hace check y la remueve.
     }
 
-    function eliminarTarea(checkbox) {
+    function eliminarTarea(checkbox, lista) {
         var tarea = $(checkbox).parent("li").find("label").text(); // Trae el id especifico de cada li de tarea.
         var index = tareas.indexOf(tarea);
 
-        tareas.splice(index, 1);
-        localStorage.setItem("tareas", JSON.stringify(tareas));
+        if(lista == "pendientes") {
+            tareas.splice(index, 1);
+            localStorage.setItem("tareas", JSON.stringify(tareas));
+        } else if(lista == "completadas") {
+            tareas_terminadas.splice(index, 1);
+            localStorage.setItem("tareas_terminadas", JSON.stringify(tareas_terminadas));
+        }
 
         $(checkbox).parent("li").remove(); //Selecciona la tarea en especifica a la que se le hace check y la remueve.
     }
